24a08deed20225294b9213ec23a0614f622c27cc
liambogenholm <bogenholm-l@webmail.uwinnipeg.ca>
2024-10-29 14:09:34 -0500

    [updates] some initial UI work

---
+++ app/home/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+function Home() {
+  return (
+    <div className="bg-green-800">
+      <p>HOME</p>
+    </div>
+  );
+}
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "main-yellow": "#C69F4B",
+        "gradient-yellow": "#EAD8B1",
       },
     },
   },
